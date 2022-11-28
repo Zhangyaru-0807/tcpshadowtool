ea62376e49bcfdcd3342d21d66800c0449f01eef
zhangyaru <2297195082@qq.com>
2022-11-28 10:14:26 +0800

    增加TestPgConnection期望结果验证

--- pgtest/pgconn_test.go
+++ pgtest/pgconn_test.go
@@ -1,17 +1,32 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPgConnection(t *testing.T) {
+	t.Parallel()
+
 	//连接数据库
 	conn, err := db_connect()
 	if err != nil {
 		t.Errorf("error连接数据库")
 	} //程序运行结束时关闭连接
-	conn.Close()
+	defer conn.Close()
+	
+	var result int
+	err = conn.QueryRow("select 1 +1").Scan(&result)
+	if err != nil {
+		t.Fatalf("QueryRow Scan unexpectedly failed: %v", err)
+	}
+	if result != 2 {
+		t.Errorf("bad result: %d", result)
+	}
 }
 
 func TestPgCreatetb(t *testing.T) {
+	t.Parallel()
+
 	conn, err := db_connect()
 	if err != nil {
 		t.Errorf("error连接数据库")
@@ -21,6 +36,8 @@ func TestPgCreatetb(t *testing.T) {
 }
 
 func TestPgInsertLine(t *testing.T) {
+	t.Parallel()
+
 	conn, err := db_connect()
 	if err != nil {
 		t.Errorf("error连接数据库")
@@ -30,6 +47,8 @@ func TestPgInsertLine(t *testing.T) {
 }
 
 func TestPgSelectLine(t *testing.T) {
+	t.Parallel()
+
 	conn, err := db_connect()
 	if err != nil {
 		t.Errorf("error连接数据库")
