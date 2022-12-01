e59db94dde5183350270a83f7da6ebd9ea214641
Martin Wang <wangxuesong@gmail.com>
2022-12-01 12:21:01 +0800

    first test case

--- pgfortune/server_test.go
+++ pgfortune/server_test.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"github.com/jackc/pgx/v5/pgproto3"
+	"bytes"
 	"net"
 	"os/exec"
 	"testing"
+
+	"github.com/jackc/pgx/v5/pgproto3"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPStartupmessage(t *testing.T) {
-	//assert := assert.New(t)
+	assert := assert.New(t)
 	conn, write := net.Pipe()
 	startupmesage := &pgproto3.StartupMessage{
 		ProtocolVersion: 196608,
@@ -24,24 +27,35 @@ func TestPStartupmessage(t *testing.T) {
 	start := startupmesage.Encode(nil)
 	backend := &PgFortuneBackend{
 		backend: pgproto3.NewBackend(write, write), //io.reader和io.writer
-		conn:    conn,
+		conn:    write,
 		responder: func() ([]byte, error) {
 			return exec.Command("sh", "", options.responseCommand).CombinedOutput()
 		},
 	}
 	go func() {
-		_, err := backend.conn.Write(start)
-		if err != nil {
-			t.Error("出错了")
-		}
-	}()
-	go func() {
 		err := backend.handleStartup()
 		if err != nil {
 			t.Error("出错了")
 		}
-		t.Error("why跑不到这里")
+		//t.Error("why跑不到这里")
 	}()
+	func() {
+		_, err := conn.Write(start)
+		if err != nil {
+			t.Error("出错了")
+		}
+	}()
+	buff := make([]byte, 16384)
+	buf := bytes.NewBuffer(buff)
+	_, err := conn.Read(buf.Bytes())
+	assert.Nil(err)
+	front := pgproto3.NewFrontend(buf, nil)
+	msg, err := front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.AuthenticationOk{}, msg)
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.ParameterStatus{}, msg)
 	//m := backend.backend.Send
 	//pgproto3.Backend{m}
 	//assert.Equal(msg, msg, "yinggaixiangdeng")
