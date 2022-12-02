9b04a598699f299cd2f553311959643525a46013
zhangyaru <2297195082@qq.com>
2022-12-02 11:07:06 +0800

    modify TestPgBridgeadd TestPgPBDES 50%

--- pgfortune/server_test.go
+++ pgfortune/server_test.go
@@ -26,28 +26,10 @@ func TestPgBridge(t *testing.T) {
 		},
 	}
 	start := startupmesage.Encode(nil)
-	//query := &pgproto3.Query{String: "selet * from test"}
-	//q := query.Encode(nil)
-	backend := &PgFortuneBackend{
-		backend: pgproto3.NewBackend(conn, conn), //io.reader和io.writer
-		conn:    conn,
-		responder: func() ([]byte, error) {
-			return exec.Command("sh", "", options.responseCommand).CombinedOutput()
-		},
+	_, err = conn.Write(start)
+	if err != nil {
+		t.Error("出错了")
 	}
-
-	go func() {
-		err = backend.Run()
-		if err != nil {
-			t.Error("出错了")
-		}
-	}()
-	func() {
-		_, err := conn.Write(start)
-		if err != nil {
-			t.Error("出错了")
-		}
-	}()
 	buff := make([]byte, 16384)  //创建buffer
 	buf := bytes.NewBuffer(buff) //初始化buffer
 	_, err = conn.Read(buf.Bytes())
@@ -59,6 +41,9 @@ func TestPgBridge(t *testing.T) {
 	msg, err = front.Receive()
 	assert.Nil(err)
 	assert.IsType(&pgproto3.ParameterStatus{}, msg)
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.ReadyForQuery{}, msg)
 }
 
 /*
@@ -253,4 +238,105 @@ func TestPgParse(t *testing.T) {
 	msg, err = front.Receive()
 	assert.Nil(err)
 	assert.IsType(&pgproto3.ParseComplete{}, msg)
+}
+func TestPgPBDES(t *testing.T) {
+	assert := assert.New(t)
+	conn, write := net.Pipe()
+	startupmesage := &pgproto3.StartupMessage{
+		ProtocolVersion: 196608,
+		Parameters: map[string]string{
+			"DateStyle":          "ISO",
+			"TimeZone":           "Asia/Shanghai",
+			"client_encoding":    "UTF8",
+			"database":           "postgres",
+			"extra_float_digits": "2",
+			"user":               "postgres",
+		},
+	}
+	start := startupmesage.Encode(nil)
+	parse := &pgproto3.Parse{
+		Name:          "",
+		Query:         "selet * from test",
+		ParameterOIDs: nil,
+	}
+	p := parse.Encode(nil)
+	bind := &pgproto3.Bind{
+		DestinationPortal:    "",
+		PreparedStatement:    "",
+		ParameterFormatCodes: nil,
+		Parameters:           nil,
+		ResultFormatCodes:    nil,
+	}
+	p = bind.Encode(p)
+	describe := &pgproto3.Describe{
+		ObjectType: 'P',
+		Name:       "",
+	}
+	p = describe.Encode(p)
+	execute := &pgproto3.Execute{
+		Portal:  "",
+		MaxRows: 0,
+	}
+	p = execute.Encode(p)
+	sync := &pgproto3.Sync{}
+	p = sync.Encode(p)
+	backend := &PgFortuneBackend{
+		backend: pgproto3.NewBackend(write, write), //io.reader和io.writer
+		conn:    write,
+		responder: func() ([]byte, error) {
+			return exec.Command("sh", "", options.responseCommand).CombinedOutput()
+		},
+	}
+	go func() {
+		err := backend.Run()
+		if err != nil {
+			t.Error("出错了")
+		}
+	}()
+	func() {
+		_, err := conn.Write(start)
+		if err != nil {
+			t.Error("出错了")
+		}
+	}()
+	buff := make([]byte, 16384)  //创建buffer
+	buf := bytes.NewBuffer(buff) //初始化buffer
+	_, err := conn.Read(buf.Bytes())
+	assert.Nil(err)
+	front := pgproto3.NewFrontend(buf, nil)
+	msg, err := front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.AuthenticationOk{}, msg)
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.ParameterStatus{}, msg)
+
+	func() {
+		_, err := conn.Write(p)
+		if err != nil {
+			t.Error("出错了")
+		}
+	}()
+	_, err = conn.Read(buf.Bytes())
+	assert.Nil(err)
+	front = pgproto3.NewFrontend(buf, nil)
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.ParseComplete{}, msg)
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.BindComplete{}, msg)
+
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.RowDescription{}, msg)
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.DataRow{}, msg)
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.CommandComplete{}, msg)
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.ReadyForQuery{}, msg)
 }
