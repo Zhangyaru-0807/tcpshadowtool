4270553292070e7a7541e0bfbf2ff4c1a46a7478
zhangyaru <2297195082@qq.com>
2022-12-06 10:16:22 +0800

    testBIND

--- bridgetool/bridgetool_test.go
+++ bridgetool/bridgetool_test.go
@@ -142,6 +142,59 @@ func TestBridgeParse(t *testing.T) {
 	assert.IsType(&pgproto3.ParseComplete{}, msg)
 }
 
+func TestBridgeBind(t *testing.T) {
+	assert := assert.New(t)
+	conn, err := net.Dial("tcp4", "127.0.0.1:11088")
+	assert.Nil(err)
+	startupmesage := &pgproto3.StartupMessage{
+		ProtocolVersion: 196608,
+		Parameters: map[string]string{
+			"DateStyle":          "ISO",
+			"TimeZone":           "Asia/Shanghai",
+			"client_encoding":    "UTF8",
+			"database":           "postgres",
+			"extra_float_digits": "2",
+			"user":               "postgres",
+		},
+	}
+	start := startupmesage.Encode(nil)
+	_, err = conn.Write(start)
+	if err != nil {
+		t.Error("出错了")
+	}
+
+	front := pgproto3.NewFrontend(conn, nil)
+	msg, err := front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.AuthenticationOk{}, msg)
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.ParameterStatus{}, msg)
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.BackendKeyData{}, msg)
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.ReadyForQuery{}, msg)
+
+	buffer := (&pgproto3.Bind{
+		DestinationPortal:    "",
+		PreparedStatement:    "",
+		ParameterFormatCodes: nil,
+		Parameters:           nil,
+		ResultFormatCodes:    nil,
+	}).Encode(nil)
+	_, err = conn.Write(buffer)
+	if err != nil {
+		t.Error("出错了")
+	}
+
+	front = pgproto3.NewFrontend(conn, nil)
+	msg, err = front.Receive()
+	assert.Nil(err)
+	assert.IsType(&pgproto3.BindComplete{}, msg)
+}
+
 func TestBridgeNOpen(t *testing.T) {
 	assert := assert.New(t)
 	conn, err := net.Dial("tcp4", "127.0.0.1:11088")
